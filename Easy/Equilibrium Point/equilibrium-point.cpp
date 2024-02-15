cb7da64104665753f0e7a46c091cc1eaf3ca27b0
KunalAgrawal <111216500+Kunal3453@users.noreply.github.com>
2024-02-15 06:01:11 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Equilibrium Point/equilibrium-point.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+#include <iostream>
+using namespace std;
+
+
+// } Driver Code Ends
+
+#include<bits/stdc++.h>
+class Solution{
+    public:
+    // Function to find equilibrium point in the array.
+    // a: input array
+    // n: size of array
+    int equilibriumPoint(long long a[], int n) {
+    
+        int right=accumulate(a,a+n,0);
+        int left=0;
+        int start=0;
+        for(int i=0;i<n;i++)
+        {
+            right-=a[i];
+            if(left==right)
+            {
+                return i+1;
+            }
+            left+=a[i];
+            
+        }
+        return -1;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+
+int main() {
+
+    long long t;
+    
+    //taking testcases
+    cin >> t;
+
+    while (t--) {
+        long long n;
+        
+        //taking input n
+        cin >> n;
+        long long a[n];
+
+        //adding elements to the array
+        for (long long i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        
+        Solution ob;
+
+        //calling equilibriumPoint() function
+        cout << ob.equilibriumPoint(a, n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
