a31d9530e984f2a06884fc57bac8ebc3819e0dd0
KunalAgrawal <111216500+Kunal3453@users.noreply.github.com>
2024-06-11 21:38:58 +0530

    Updated Solution - GfG to GitHub

--- Medium/Kth smallest element/kth-smallest-element.cpp
+++ Medium/Kth smallest element/kth-smallest-element.cpp
@@ -14,24 +14,16 @@ class Solution{
     // r : ending index of the array i.e size-1
     // k : find kth smallest element and return using this function
     int kthSmallest(int arr[], int l, int r, int k) {
-        //code here
-        sort(arr,arr+r);
-        priority_queue<int>pq;
-        for(int i=0;i<k;i++)
-        {
-            pq.push(arr[i]);
-        }
-        for(int i=k;i<=r;i++)
-        {
-            if(pq.top()>arr[i])
-            {
-                pq.pop();
-                 pq.push(arr[i]);
-                
-            }
-           
-        }
-        return pq.top();
+       priority_queue<int>pq;
+       for(int i=0;i<=r;i++)
+       {
+           pq.push(arr[i]);
+           if(pq.size()>k)
+           {
+               pq.pop();
+           }
+       }
+       return pq.top();
     }
 };
 
